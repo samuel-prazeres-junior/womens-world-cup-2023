3fac3167681e409c499f02a5481e11bc4a941e39
Samuel Prazeres Junior <61557547+samuel-prazeres-junior@users.noreply.github.com>
2024-02-20 09:28:18 -0300

    Update

--- src/main/kotlin/io/github/womensworldcup2023/domain/repository/TeamRepository.kt
+++ src/main/kotlin/io/github/womensworldcup2023/domain/repository/TeamRepository.kt
@@ -2,6 +2,8 @@ package io.github.womensworldcup2023.domain.repository
 
 import io.github.womensworldcup2023.domain.model.Team
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface TeamRepository: JpaRepository<Team, String> {
 }
